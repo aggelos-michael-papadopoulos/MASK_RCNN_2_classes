f1f0ab12537aeb42d3c2a5adbcf499ffe4d8592a
papadopoulos Aggelos-Michael <65830412+aggelos-michael-papadopoulos@users.noreply.github.com>
2022-03-28 00:28:11 +0300

    Update utils.py

--- mrcnn/utils.py
+++ mrcnn/utils.py
@@ -3,7 +3,7 @@ Mask R-CNN
 Common utility functions and classes.
 
 Copyright (c) 2017 Matterport, Inc.
-Licensed under the MIT License (see LICENSE for details)
+Licensed under the MIT License  (see LICENSE for details)
 Written by Waleed Abdulla
 """
 
